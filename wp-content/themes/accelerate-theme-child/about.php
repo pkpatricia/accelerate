4b458e4138919391483496d4bb857c71550c54f7
pkpatricia <pk.patricia@gmail.com>
2015-03-22 18:28:28 -0500

    added about page

---
+++ wp-content/themes/accelerate-theme-child/about.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: About Page
+ *
+ * 
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="site-content">
+    <div id="content" role="main">
+      <?php while ( have_posts() ) : the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile; // end of the loop. ?>
+
+    </div><!-- #content -->
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>
